5650415b9c2949cb49faabde8173ea780695eb87
ZharovVsevolod <wsewolod@mail.ru>
2023-11-20 14:35:18 +0300

    now in format torch.tensor

--- load_data.py
+++ load_data.py
@@ -2,6 +2,8 @@ from datasets import load_dataset, load_from_disk
 from vit import config
 import cv2
 import matplotlib.pyplot as plt
+import torch
+import einops
 
 def load_data_beans():
     ds = load_dataset(
@@ -25,12 +27,14 @@ def load_data_beans():
 # load_data_beans()
 
 ds = load_from_disk("./dataset/train")
-print(ds[0])
-
-print(ds[0]["image"])
+# print(ds[0])
+# print(ds[0]["image"])
 
 image = cv2.imread(ds[0]["image_file_path"])
-print(image)
-image = image.astype("float32") / 255.0
+image = image.astype("float64") / 255.0
 plt.imshow(image)
 plt.show()
+
+image_t = torch.tensor(image)
+image_t = einops.rearrange(image_t, "h w c -> c w h", c=3)
+print(image_t.shape)
